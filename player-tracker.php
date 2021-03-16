435b40146479ac32b9e62f4fb262beed3eb0d42c
Zane DeVault <zanedevault@gmail.com>
2021-03-15 19:00:29 -0700

    add main plugin file header comment

---
+++ player-tracker.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Player Tracker
+ *
+ * This is the project that I'm using to learn OOP, Test Driven Development, phpDocumentor, Composer and probably some JavaScript and maybe even a little React.
+ *
+ * @package           player-tracker
+ * @author            Zane DeVault
+ * @copyright         2021 Zane DeVault
+ * @license           GPL-2.0-or-later
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Player Tracker
+ * Description:       This is the project that I'm using to learn OOP, Test Driven Development, phpDocumentor, Composer and probably some JavaScript and maybe even a little React.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Zane DeVault
+ * Author URI:        https://zanedevault.com
+ * Text Domain:       zrdv-pt
+ * License:           GPL v2 or later
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ */
